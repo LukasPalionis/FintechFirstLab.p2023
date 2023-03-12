d55bca23809bf6e59a15055384842c696868b9bb
Lukas Palionis <lkpalionis@gmail.com>
2023-03-12 20:54:43 +0200

    Added change Text functionality

--- app/src/main/java/com/example/fintechfirstlabp2023/MainActivity.java
+++ app/src/main/java/com/example/fintechfirstlabp2023/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.fintechfirstlabp2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView TvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.TvMain = findViewById(R.id.TvMain);
+    }
+
+    public void OnBtnClick(View view) {
+        this.TvMain.setText (getString(R.string.changed_lable_caption));
     }
 }
